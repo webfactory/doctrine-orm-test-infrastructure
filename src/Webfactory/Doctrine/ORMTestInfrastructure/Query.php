ea340f3f5d4914362e7262e8435889e91d984732
Matthias Molitor <mmo@webfactory.de>
2015-01-16 17:47:20 +0100

    added method stubs (#3)

--- src/Webfactory/Doctrine/ORMTestInfrastructure/Query.php
+++ src/Webfactory/Doctrine/ORMTestInfrastructure/Query.php
@@ -4,8 +4,26 @@ namespace Webfactory\Doctrine\ORMTestInfrastructure;
 
 /**
  * Represents a query that has been executed.
+ *
+ * This class is designed to be populated by the data that is gathered by the DebugStack logger.
+ *
+ * @see \Doctrine\DBAL\Logging\DebugStack
  */
 class Query
 {
+    // ('sql' => $sql, 'params' => $params, 'types' => $types, 'executionMS' => 0
+    public function getSql()
+    {
 
+    }
+
+    public function getParams()
+    {
+
+    }
+
+    public function getExecutionTimeInSeconds()
+    {
+
+    }
 }
