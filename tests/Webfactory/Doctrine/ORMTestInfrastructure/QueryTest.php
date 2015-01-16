e68f6f11a742275d01ded7057939a095f02619f7
Matthias Molitor <mmo@webfactory.de>
2015-01-16 18:04:33 +0100

    added TestCase stub (#3)

---
+++ tests/Webfactory/Doctrine/ORMTestInfrastructure/QueryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Webfactory\Doctrine\ORMTestInfrastructure;
+
+/**
+ * Tests the value object that holds query data.
+ */
+class QueryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetSqlReturnsCorrectValue()
+    {
+
+    }
+
+    public function testGetParamsReturnsCorrectValue()
+    {
+
+    }
+
+    public function testGetExecutionTimeInSecondsReturnsCorrectValue()
+    {
+
+    }
+
+    public function testQueryObjectProvidesStringRepresentation()
+    {
+
+    }
+}
