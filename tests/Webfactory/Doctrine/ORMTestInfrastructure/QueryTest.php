ea0067e816fea7c003072c06f24b4a1930e40473
Matthias Molitor <mmo@webfactory.de>
2015-01-16 18:18:22 +0100

    added comments (#3)

--- tests/Webfactory/Doctrine/ORMTestInfrastructure/QueryTest.php
+++ tests/Webfactory/Doctrine/ORMTestInfrastructure/QueryTest.php
@@ -7,21 +7,33 @@ namespace Webfactory\Doctrine\ORMTestInfrastructure;
  */
 class QueryTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Checks if the correct SQL is returned by the query object.
+     */
     public function testGetSqlReturnsCorrectValue()
     {
 
     }
 
+    /**
+     * Checks if the query parameters are returned correctly.
+     */
     public function testGetParamsReturnsCorrectValue()
     {
 
     }
 
+    /**
+     * Ensures that the correct execution time is returned.
+     */
     public function testGetExecutionTimeInSecondsReturnsCorrectValue()
     {
 
     }
 
+    /**
+     * Checks if the query object can be used to retrieve a string representation of the query.
+     */
     public function testQueryObjectProvidesStringRepresentation()
     {
 
