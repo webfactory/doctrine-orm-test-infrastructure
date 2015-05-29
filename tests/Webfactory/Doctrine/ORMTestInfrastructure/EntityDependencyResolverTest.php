4efa27ec7fe6692da5ab273f1b6e46d0c2c492ac
Matthias Molitor <mmo@webfactory.de>
2015-05-29 16:25:53 +0200

    added comments (#4)

--- tests/Webfactory/Doctrine/ORMTestInfrastructure/EntityDependencyResolverTest.php
+++ tests/Webfactory/Doctrine/ORMTestInfrastructure/EntityDependencyResolverTest.php
@@ -14,26 +14,47 @@ namespace Webfactory\Doctrine\ORMTestInfrastructure;
  */
 class EntityDependencyResolverTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Ensures that the resolver is traversable.
+     */
     public function testResolverIsTraversable()
     {
 
     }
 
+    /**
+     * Checks if the resolved set contains the initially provided entity classes.
+     */
     public function testSetContainsProvidedEntityClasses()
     {
 
     }
 
+    /**
+     * Checks if entities that are directly associated to the initially provided entities
+     * are contained in the resolved set.
+     */
     public function testSetContainsEntityClassesThatAreDirectlyConnectedToInitialSet()
     {
 
     }
 
+    /**
+     * Ensures that entities, which are connected via other associated entities,
+     * are contained in the generated set.
+     *
+     * Example:
+     *
+     *     A -> B -> C
+     */
     public function testSetContainsIndirectlyConnectedEntityClasses()
     {
 
     }
 
+    /**
+     * Ensures that the resolver can handle dependency cycles.
+     */
     public function testResolverCanHandleDependencyCycles()
     {
 
