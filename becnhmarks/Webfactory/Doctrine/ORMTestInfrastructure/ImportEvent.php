ef4514b1179ab873da785fbac3428ae9d65aebb2
Matthias Molitor <mmo@webfactory.de>
2015-07-25 18:59:32 +0200

    added stubs for different benchmark aspects

---
+++ becnhmarks/Webfactory/Doctrine/ORMTestInfrastructure/ImportEvent.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * (c) webfactory GmbH <info@webfactory.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webfactory\Doctrine\ORMTestInfrastructure;
+
+use Athletic\AthleticEvent;
+
+/**
+ * Compares the different methods that can be used to import test data.
+ */
+class ImportEvent extends AthleticEvent
+{
+
+}
---
+++ becnhmarks/Webfactory/Doctrine/ORMTestInfrastructure/InfrastructureSetUpEvent.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * (c) webfactory GmbH <info@webfactory.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webfactory\Doctrine\ORMTestInfrastructure;
+
+use Athletic\AthleticEvent;
+
+/**
+ * Compares the different methods that can be used to setup the infrastructure.
+ */
+class InfrastructureSetUpEvent extends AthleticEvent
+{
+
+}
