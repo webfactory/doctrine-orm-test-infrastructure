af506a7cedede238dd360013dfdf920a006e87cc
Matthias Molitor <mmo@webfactory.de>
2015-07-26 11:32:03 +0200

    added import benchmark stubs

--- benchmarks/Webfactory/Doctrine/ORMTestInfrastructure/ImportEvent.php
+++ benchmarks/Webfactory/Doctrine/ORMTestInfrastructure/ImportEvent.php
@@ -16,5 +16,43 @@ use Athletic\AthleticEvent;
  */
 class ImportEvent extends AthleticEvent
 {
+    public function import10EntitiesFromList()
+    {
 
+    }
+
+    public function import100EntitiesFromList()
+    {
+
+    }
+
+    public function import10EntitiesViaCallback()
+    {
+
+    }
+
+    public function import100EntitiesViaCallback()
+    {
+
+    }
+
+    public function import10EntitiesViaPhpFile()
+    {
+
+    }
+
+    public function import100EntitiesViaPhpFile()
+    {
+
+    }
+
+    public function import10EntitiesReturnedByPhpFile()
+    {
+
+    }
+
+    public function import100EntitiesReturnedByPhpFile()
+    {
+
+    }
 }
