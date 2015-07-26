77daa308e10e003a5ffdb544ce3569bc7986c5e0
Matthias Molitor <mmo@webfactory.de>
2015-07-26 10:53:17 +0200

    defined infrastructure creation benchmarks

--- benchmarks/Webfactory/Doctrine/ORMTestInfrastructure/InfrastructureSetUpEvent.php
+++ benchmarks/Webfactory/Doctrine/ORMTestInfrastructure/InfrastructureSetUpEvent.php
@@ -13,36 +13,78 @@ use Athletic\AthleticEvent;
 
 /**
  * Compares the different methods that can be used to setup the infrastructure.
+ *
+ * After creation of the infrastructure object, the entity manager must be retrieved
+ * as the initialization is applied lazy.
  */
 class InfrastructureSetUpEvent extends AthleticEvent
 {
+    /**
+     * @iterations 100
+     */
     public function simpleEntityWithAssociationDetection()
     {
-
+        $infrastructure = ORMInfrastructure::createWithDependenciesFor(
+            '\Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\TestEntity'
+        );
+        $infrastructure->getEntityManager();
     }
 
+    /**
+     * @iterations 100
+     */
     public function simpleEntityWithoutAssociationDetection()
     {
-
+        $infrastructure = ORMInfrastructure::createOnlyFor(
+            '\Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\TestEntity'
+        );
+        $infrastructure->getEntityManager();
     }
 
+    /**
+     * @iterations 100
+     */
     public function singleAssociationEntityWithAssociationDetection()
     {
-
+        $infrastructure = ORMInfrastructure::createWithDependenciesFor(
+            '\Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\TestEntityWithDependency'
+        );
+        $infrastructure->getEntityManager();
     }
 
+    /**
+     * @iterations 100
+     */
     public function singleAssociationEntityExplicitAssociationDefinition()
     {
-
+        $infrastructure = ORMInfrastructure::createOnlyFor(array(
+            '\Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\TestEntityWithDependency',
+            '\Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\ReferencedEntity'
+        ));
+        $infrastructure->getEntityManager();
     }
 
+    /**
+     * @iterations 100
+     */
     public function complexEntityWithAssociationDetection()
     {
-
+        $infrastructure = ORMInfrastructure::createWithDependenciesFor(
+            '\Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\ChainReferenceEntity'
+        );
+        $infrastructure->getEntityManager();
     }
 
+    /**
+     * @iterations 100
+     */
     public function complexEntityWithExplicitAssociationDefinition()
     {
-
+        $infrastructure = ORMInfrastructure::createOnlyFor(array(
+            '\Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\ChainReferenceEntity',
+            '\Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\TestEntityWithDependency',
+            '\Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\ReferencedEntity'
+        ));
+        $infrastructure->getEntityManager();
     }
 }
