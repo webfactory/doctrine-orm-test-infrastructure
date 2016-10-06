a9c1a5fa342edec5eae61131176781ee36f61dbe
Matthias Molitor <mmo@webfactory.de>
2016-10-06 15:31:31 +0200

    added tests

--- tests/Webfactory/Doctrine/Config/FileDatabaseConnectionConfigurationTest.php
+++ tests/Webfactory/Doctrine/Config/FileDatabaseConnectionConfigurationTest.php
@@ -36,26 +36,74 @@ class FileDatabaseConnectionConfigurationTest extends \PHPUnit_Framework_TestCas
     public function testWorksWithInfrastructure()
     {
         $configuration = new FileDatabaseConnectionConfiguration();
-        $infrastructure = ORMInfrastructure::createOnlyFor(array(
-            'Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\TestEntity'
-        ), $configuration);
+        $infrastructure = $this->createInfrastructure($configuration);
 
         $this->setExpectedException(null);
         $infrastructure->import(new TestEntity());
     }
 
-    public function testDatabaseIsRemovedWhenInfrastructureIsDestroyed()
+    public function testDatabaseFileIsCreated()
+    {
+        $configuration = new FileDatabaseConnectionConfiguration();
+
+        $infrastructure = $this->createInfrastructure($configuration);
+        $infrastructure->import(new TestEntity());
+
+        $this->assertFileExists($configuration->getDatabaseFile());
+    }
+
+    public function testDatabaseFileIsRemovedWhenInfrastructureIsDestroyed()
     {
         $configuration = new FileDatabaseConnectionConfiguration();
         $databaseFile  = $configuration->getDatabaseFile();
 
-        $infrastructure = ORMInfrastructure::createOnlyFor(array(
-            'Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\TestEntity'
-        ), $configuration);
+        $infrastructure = $this->createInfrastructure($configuration);
         $infrastructure->import(new TestEntity());
 
         $configuration = null;
         $infrastructure = null;
         $this->assertFileNotExists($databaseFile);
+    }
+
+    public function testWorksIfFileAlreadyExists()
+    {
+        $databaseFile = tempnam(sys_get_temp_dir(), 'db-');
+        file_put_contents($databaseFile, 'my irrelevant file content');
+        $configuration = new FileDatabaseConnectionConfiguration($databaseFile);
+        $infrastructure = $this->createInfrastructure($configuration);
+
+        $this->setExpectedException(null);
+        $infrastructure->import(new TestEntity());
+    }
+
+    public function testDoesNotRemoveFileIfAutoCleanUpIsDisabled()
+    {
+        $configuration = new FileDatabaseConnectionConfiguration(null, false);
+        $databaseFile  = $configuration->getDatabaseFile();
+
+        $infrastructure = $this->createInfrastructure($configuration);
+        $infrastructure->import(new TestEntity());
+
+        $configuration = null;
+        $infrastructure = null;
+        $this->assertFileExists($databaseFile);
+        unlink($databaseFile);
+    }
+
+    /**
+     * Creates a new infrastructure with the given connection configuration.
+     *
+     * @param ConnectionConfiguration $configuration
+     * @return ORMInfrastructure
+     */
+    private function createInfrastructure(ConnectionConfiguration $configuration)
+    {
+        $infrastructure = ORMInfrastructure::createOnlyFor(
+            array(
+                'Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\TestEntity'
+            ),
+            $configuration
+        );
+        return $infrastructure;
     }
 }
