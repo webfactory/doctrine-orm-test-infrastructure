75141e22f7da57a9aa61a0ef41b9b09e697d4158
Matthias Molitor <mmo@webfactory.de>
2017-03-21 15:12:53 +0100

    moved annotations to same directory

--- tests/Webfactory/Doctrine/ORMTestInfrastructure/_files/ORMInfrastructure/Annotation/CustomAnnotation.php
+++ tests/Webfactory/Doctrine/ORMTestInfrastructure/_files/ORMInfrastructure/Annotation/CustomAnnotation.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) webfactory GmbH <info@webfactory.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\Annotation;
 
 /**
--- tests/Webfactory/Doctrine/ORMTestInfrastructure/_files/ORMInfrastructure/Annotation/TestAnnotation.php
+++ tests/Webfactory/Doctrine/ORMTestInfrastructure/_files/ORMInfrastructure/Annotation/TestAnnotation.php
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest;
+namespace Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\Annotation;
 
 /**
  * An annotation that is used in the test scenarios.
