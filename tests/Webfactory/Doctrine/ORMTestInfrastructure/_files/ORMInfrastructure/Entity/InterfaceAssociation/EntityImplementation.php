0b0965a890e602dd69cb0b4ee95678b3274670a4
Matthias Molitor <mmo@webfactory.de>
2017-05-11 16:59:21 +0200

    annotated test entity

--- tests/Webfactory/Doctrine/ORMTestInfrastructure/_files/ORMInfrastructure/Entity/InterfaceAssociation/EntityImplementation.php
+++ tests/Webfactory/Doctrine/ORMTestInfrastructure/_files/ORMInfrastructure/Entity/InterfaceAssociation/EntityImplementation.php
@@ -3,9 +3,13 @@
 namespace Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest;
 
 use Webfactory\Doctrine\ORMTestInfrastructure\ORMInfrastructureTest\InterfaceAssociation\EntityInterface;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Implements an interface that is used in an association.
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="entity_implementing_referenced_interface")
  */
 class EntityImplementation implements EntityInterface
 {
