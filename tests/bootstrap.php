ed804babb30dee0a67ed4d67e14c4699a5ad0c4b
Matthias Pigulla <mp@webfactory.de>
2025-11-18 12:55:02 +0100

    Expand test matrix, raise minimum requirements (#61)

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\ErrorHandler\DebugClassLoader;
-
-require_once __DIR__.'/../vendor/autoload.php';
-
-DebugClassLoader::enable();
